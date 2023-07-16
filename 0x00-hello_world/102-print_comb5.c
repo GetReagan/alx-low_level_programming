c2ce97cab9999874424986c258aa384ed4077a4c
GetReagan <reaganatta43@gmail.com>
2023-07-16 14:08:00 -0700

    codes that prints all possible combinations of two two-digit numbers

---
+++ 0x00-hello_world/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = '0'; i <= 99; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+		for (j = '0'; j <= 99; j++)
+		{
+			c = j / 10;
+			d = j % 10l;
+	
+			if (a < c || (a == c && b < d))
+			 {
+				 putchar(a);
+				 putchar(b);
+				 putchar(',');
+				 putchar(c);
+				 putchar(d);
+				 
+				 if (a != '9' && b !='8')
+				 {
+					 putchar(',');
+					 putchar(' ');
+				 }
+			 }
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
