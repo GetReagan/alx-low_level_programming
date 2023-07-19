8e0d14243b17b7f366498a7158155792b199f2f0
GetReagan <reaganatta43@gmail.com>
2023-07-19 07:56:44 -0700

    Infinite loop

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: Always 0 (Succes)
+*/
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
